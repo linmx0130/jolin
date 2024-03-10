12106a271ee2fb8926f114edfef28af797e1abd6
Mengxiao Lin <linmx0130@gmail.com>
2024-03-09 21:53:30 -0800

    Mat64 specific det

--- jolin/src/det.rs
+++ jolin/src/det.rs
@@ -8,9 +8,10 @@
 
 use crate::matrix::{Matrix, LikeNumber};
 use crate::error::JolinError;
-use crate::decomp::lu::lu;
+use crate::decomp::lu::{lu, LUDecomposable};
+use crate::Mat64;
 
-/// Compute the determinate of the matrix
+/// Compute the determinant of the matrix
 pub fn det<T: Matrix>(mat: &T) -> Result<T::Elem, JolinError> {
     if mat.row() != mat.column() {
         return Err(JolinError::shape_mismatching())
@@ -28,6 +29,38 @@ pub fn det<T: Matrix>(mat: &T) -> Result<T::Elem, JolinError> {
                         Ok(detlu)
                     } else {
                         Ok(-detlu)
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// Type-specific determinant algorithm.
+trait DeterminantComputable: Matrix {
+    /// Compute the determinant of the matrix.
+    fn det(mat: &Self) -> Result<Self::Elem, JolinError>;
+}
+
+impl DeterminantComputable for Mat64 {
+    fn det(mat: &Mat64) -> Result<f64, JolinError> {
+        if mat.row() != mat.column() {
+            return Err(JolinError::shape_mismatching())
+        }
+        return match mat.row() {
+            2 => {
+                Ok(mat.elem(0, 0) * mat.elem(1, 1) - mat.elem(0, 1) * mat.elem(1, 0))
+            }
+            _ => {
+                match Mat64::lu_decomp(mat) {
+                    Err(_err) => Ok(0.0),
+                    Ok(lud) => {
+                        let detlu = diagonal_product(&lud.l) * diagonal_product(&lud.u);
+                        if permutation_order(&lud.p) % 2 == 0 {
+                            Ok(detlu)
+                        } else {
+                            Ok(-detlu)
+                        }
                     }
                 }
             }
@@ -62,8 +95,8 @@ fn permutation_order(p: &Vec<usize>) -> usize {
 #[cfg(test)]
 mod test {
     use crate::mat64;
-    use crate::det::det;
-    use crate::matrix::Matrix;
+    use crate::det::{det, DeterminantComputable};
+    use crate::matrix::{Matrix, Mat64};
     
     #[test]
     fn test_det_2x2() {
@@ -93,5 +126,25 @@ mod test {
             1.0, 2.0, 2.0, 0.0;
             0.0, 0.0, 0.0, 1.0
         ]), Ok(0.0));
+    }
+    
+    #[test]
+    fn test_mat64_det_3x3() {
+        assert_eq!(Mat64::det(&mat64![
+            1.0, 2.0, 3.0; 
+            2.0, 3.0, 1.0; 
+            2.0, 4.0, 2.0]
+        ), Ok(4.0));
+        assert_eq!(Mat64::det(&mat64![
+            1.0, 2.0, 3.0;
+            2.0, 4.0, 2.0;
+            2.0, 3.0, 1.0]
+        ), Ok(-4.0));
+        assert_eq!(Mat64::det(&mat64![
+            1.0, 0.0, 0.0, 1.0;
+            1.0, 1.0, 1.0, 1.0;
+            1.0, 2.0, 1.0, 0.0;
+            0.0, 0.0, 0.0, 1.0]
+        ), Ok(-1.0));
     }
 }
